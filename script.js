634f4ffc39a8abde2eba6aa328984b0f1cfb872b
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-04-26 21:23:12 +0200

    Fix caching issue

--- script.js
+++ script.js
@@ -12,20 +12,26 @@ let setData = async (time) => {
     console.log("Updating data...");
     let ctime = new Date();
     console.log(ctime);
-    let data = await (await fetch("https://raw.githubusercontent.com/Kanzaji/MMC-Last-Drama/data/data.json")).json();
-    if (data == NaN) {
-        console.error("Data fetch failed?!");
-        time.innerHTML = "An error has occured. Github might be down?";
-        return -1;
-    } else {
-        console.log("Data fetched!");
-        console.log(data);
-        let days = Math.floor((Date.now() - Date.parse(data.data))/86400000);
-        if (days < 0) {
-            console.error("Days are below zero! Wrong time zone lol. Resetting to zero");
-            days = 0;
+    try {
+        let data = await (await fetch("https://raw.githubusercontent.com/Kanzaji/MMC-Last-Drama/data/data.json",{cache: "no-cache"})).json();
+        if (data == NaN) {
+            console.error("Data fetch failed?!");
+            time.innerHTML = "An error has occured. Github might be down?";
+            return -1;
+        } else {
+            console.log("Data fetched!");
+            console.log(data);
+            let days = Math.floor((Date.now() - Date.parse(data.data))/86400000);
+            if (days < 0) {
+                console.error("Days are below zero! Wrong time zone lol. Resetting to zero");
+                days = 0;
+            }
+            console.log(days + " days since last drama.");
+            time.innerHTML = days;
         }
-        console.log(days + " days since last drama.");
-        time.innerHTML = days;
+    } catch (err) {
+        console.error("Data fetch failed!?");
+        console.error(err);
+        return -1;
     }
 } 
