97d0b29f82ec7ea2f90b97e9017bd4bf3caa6ebf
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-04-26 21:00:40 +0200

    Update data and some other changes (why am I doing this)

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-addEventListener("load", () => {
+addEventListener("load", async () => {
     let time = document.getElementById("time")
+    setData()
+    let main = setInterval(async () => {
+        setData()
+    }, 1800 * 1000);
+})
+
+let setData = async () => {
+    console.log("Updating data...")
     let ctime = new Date()
     console.log(ctime)
-    data = fetch("")
-})
+    let data = await (await fetch("https://raw.githubusercontent.com/Kanzaji/MMC-Last-Drama/master/data.json")).json()
+    if (data == NaN) {
+        console.error("Data fetch failed?!")
+        time.innerHTML = "An error has occured. Github might be down?"
+    } else {
+        console.log("Data fetched!")
+        console.log(data)
+        let days = Math.floor((Date.now() - Date.parse(data.data))/86400000)
+        console.log(days + " days since last drama.")
+        time.innerHTML = days
+    }
+} 
